deb7b0efda432234c4ecfd7ec4a97986eb23408d
NQuynhAnh <111996607+NQuynhAnh@users.noreply.github.com>
2022-09-11 17:27:14 +0700

    NQA_7090

---
+++ Models/Person.cs
@@ -0,0 +1,20 @@
+<<<<<<< HEAD
+namespace NguyenQuynhAnhBTH.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public int PersonAge { get; set; }
+    }
+=======
+namespace NguyenQuynhAnhBTH.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public int PersonAge { get; set; }
+    }
+>>>>>>> 40c5637e0b2e04a9870ccbe4717bc6bc08c11ac9
+}
