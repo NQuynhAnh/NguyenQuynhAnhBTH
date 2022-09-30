e09f5d06c11372c7998bf40fa5715145b818c2da
NQuynhAnh <nguyenquynh010920@gmail.com>
2022-09-30 21:23:08 +0700

    NQA_2809

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -6,19 +6,4 @@ namespace NguyenQuynhAnhBTH.Models
         public string EmployeeName { get; set; }
         public int EmployeeAge { get; set; }
     }
-<<<<<<< HEAD
 }
-=======
-}
-=======
-namespace NguyenQuynhAnhBTH.Models
-{
-    public class Employee
-    {
-        public int EmployeeID { get; set; }
-        public string EmployeeName { get; set; }
-        public int EmployeeAge { get; set; }
-    }
-}
->>>>>>> 40c5637e0b2e04a9870ccbe4717bc6bc08c11ac9
->>>>>>> deb7b0efda432234c4ecfd7ec4a97986eb23408d
